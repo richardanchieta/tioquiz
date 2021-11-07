cb2e380e20858207e3dc657c8d445f3c093592a9
Richard Anchieta <richard.anchieta@gmail.com>
2021-11-06 23:34:21 -0300

    wip: adiciona scaffold inicial

---
+++ apps/studio-cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '26915ce43aa15806c73b7bc1d3ddcce1'),
+    },
+  },
+});
